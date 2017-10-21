47ed437085dabbc29d8edfbd1d7992a56d671cb5
marton bognar <martonbognar@sch.bme.hu>
2017-10-21 17:24:57 +0200

    Add basic models

--- TripPlanner/backend/models.py
+++ TripPlanner/backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    city_name = models.CharField(max_length=255)
+    planned_days = models.IntegerField()
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+
+class Trip(models.Model):
+    name = models.CharField(max_length=255)
+    completed = models.BooleanField()
+    start_day = models.DateField()
