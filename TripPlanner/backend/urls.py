67eb2c054692174051caa2ba9bff19242c33787b
Anonymvs <dani940414@gmail.com>
2017-10-21 17:40:01 +0200

    authentication

--- TripPlanner/backend/urls.py
+++ TripPlanner/backend/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^login', views.login, name='login'),
+    url(r'^yay', views.yay, name='yay'),
+    url(r'^nay', views.nay, name='nay'),
     url(r'^', views.index, name='index'),
 ]
--- TripPlanner/backend/views.py
+++ TripPlanner/backend/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def index(request):
     return render(request, 'backend/index.html')
+
+
+def user_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('/yay')
+    else:
+        return redirect('/nay')
+
+
+def yay(request):
+    html = "<html><body> Succesful login </body></html>"
+    return HttpResponse(html)
+
+
+def nay(request):
+    html = "<html><body> Incorrect username, or password </body></html>"
+    return HttpResponse(html)
