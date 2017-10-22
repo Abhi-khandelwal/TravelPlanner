ac169c087e0a3a3252b86c469bf4a3536b922eab
Gergely Tuskó <gergely.tusko@gmail.com>
2017-10-22 03:30:32 +0200

    Work on add/remove destination feature

--- TripPlanner/backend/static/backend/js/main.js
+++ TripPlanner/backend/static/backend/js/main.js
@@ -1,16 +1,16 @@
 (function() {
 	var destinationCount = 1;
 	document.getElementById("addDestination").addEventListener("click", function(){
-	    document.getElementById("destination-container").innerHTML += `<div class="form-group">
+	    document.getElementById("destination-container").innerHTML += `<div class="form-group" id="destination-`+ destinationCount+`">
           <div class="row">
             <div class="col-4">
-              <label for="destination-1">Destination 1:</label>
+              <label for="destination-1">Destination `+ destinationCount +`:</label>
             </div>
             <div class="col-4">
-              <input type="text" class="form-control" id="destination-1" name="destination-1">
+              <input type="text" class="form-control" id="destination-name-` + destinationCount + `" name="destination-` + destinationCount + `">
             </div>
             <div class="col-2">
-              <input type="number" class="form-control" id="destination-duration-1" name="destination-duration-1" placeholder="DUR">
+              <input type="number" class="form-control" id="destination-duration-` + destinationCount + `" name="destination-duration- ` + destinationCount + `" placeholder="DUR">
             </div>
             <div class="col-2">
               <button class="btn btn-outline-danger">Delete</button>
@@ -18,5 +18,8 @@
           </div>
         </div>`;
 	    destinationCount++;
+	    document.getElementById("removeDestination-" + destinationCount).addEventListener("click", function(){
+	    	document.getElementById("destination"+ destinationCount).remove();
+		});
 	});
 })();
